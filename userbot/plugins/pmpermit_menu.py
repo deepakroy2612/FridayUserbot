571668bd55e7e11a1f6630af7aa967c9f6347437
deepakroy2612 <65287643+deepakroy2612@users.noreply.github.com>
2020-06-28 11:28:12 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -33,7 +33,7 @@ async def _(event):
                "__Let's make this smooth and let me know why you are here.__\n"
                "**Choose one of the following reasons why you are here:**\n\n"
                "`1`. To chat with my master\n"
-               "`2`. To Give Your Girl Friend Details.\n"
+               "`2`. To send nudes.\n"
 
                "`3`. To enquire something\n"
                "`4`. To request something\n")
